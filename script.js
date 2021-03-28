ee015509c71f6d7115ed325d1f4d30f49f4e9e69
Sander Meijer <sandertje1972@hotmail.com>
2021-03-28 11:41:50 +0200

    made function for deleting movies

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ for (i=0; i < movies.length; i++) {
     const newLi = document.createElement('li');
     const newA = document.createElement('a');
     const newImg = document.createElement('img');
-    //href en target="_blank" meegeven aan de a
+    //href meegeven aan de a en laten openen in nieuw scherm
     newA.href=("https://www.imdb.com/title/" + movies[i].imdbID);
     newA.target="_blank";
     //src meegeven aan de img
@@ -19,4 +19,18 @@ for (i=0; i < movies.length; i++) {
     //plaats de li in de ul
     filteredMovies.appendChild(newLi);
 };
+
+
+//voordat je een bepaalde lijst gaat krijgen, verwijder je alle films
+removeMovies = function() {
+    //het aantal films in je ul
+    const numberOfMovies = filteredMovies.getElementsByTagName("li").length;
+    for (i = 0; i < numberOfMovies; i++) {
+        //eerste film in de lijst
+        const firstMovie = filteredMovies.getElementsByTagName("li")[0];        
+        filteredMovies.removeChild(firstMovie);
+    };
+};
+
+
 
